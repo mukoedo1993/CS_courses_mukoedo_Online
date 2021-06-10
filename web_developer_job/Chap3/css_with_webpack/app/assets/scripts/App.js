6cbea0e3b45c24e0cc018f27da27aaf5263dfbfa
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-06-10 13:49:47 -0700

    part of sec15

---
+++ web_developer_job/Chap3/css_with_webpack/app/assets/scripts/App.js
@@ -0,0 +1,11 @@
+
+//COURSE 15TH
+import '../styles/styles.css'
+//Without any configurations, webpaqck only understands JS file, 
+// Go to npm store to install package.
+// cd *with_webpack && npm install css-loader style-loader --save-dev
+// END OF COURSE 15TH NEW MODIFICATION
+
+
+alert("Hello, this is just a test!!!?")
+// If you open  up the HTML file loading this JS file, a window with this caption will pop up.
