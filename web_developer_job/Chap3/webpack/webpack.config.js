07d7a9353752e5dc781a56f9a057e9c980441532
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-06-10 12:49:26 -0700

    13th and 14th courses

--- web_developer_job/Chap3/webpack/webpack.config.js
+++ web_developer_job/Chap3/webpack/webpack.config.js
@@ -1,11 +1,20 @@
-const path = require('path') // import path
-module.exports = {
-    entry: './app/assets/scripts/App.js',
-    output: {
+const path = require('path') // import path (which is a part of nodejs lib.)
+
+module.exports = {  // When webpack loads this file, it knows the JS object we just created is what should be exported and used.
+
+    entry: './app/assets/scripts/App.js', // It points to the poath of the JS file we want webpack to watch, process and bundle.
+
+
+    output: {   // control what the bundle file should e named 
         filename: 'bundled.js',
-        path: path.resolve(__dirname, 'app') // webpack requires absoulte path to work. we need to manually import nodejs 
+        path: path.resolve(__dirname, 'app') // webpack requires absoulte path to work. we need to manually import nodejs
+                                             // This makes sure that no matter what operating system you're on, windows, Mac, or Linux, it will be 
+                                             // able to generate an absolute path.
+
         // path package.
     },
+
+
     mode: 'development',
     watch: true // webpack will keep actively watching us, we do not need to run any other code again on terminal...
 
